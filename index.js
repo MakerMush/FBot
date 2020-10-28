ac68e0bfd38f8336741b5f5e4a20ed3702c4cdf5
MakerMush <71267581+MakerMush@users.noreply.github.com>
2020-10-28 09:24:33 -0400

    better ggez

--- index.js
+++ index.js
@@ -294,7 +294,9 @@ function checkForFReactions(message) {
 
 function checkGGEZ(message) {
 	if (message.content === 'ggez') {
-		message.channel.send(ggEZResponses[Math.random(ggEZResponses.length())]);
+		console.log('there are ' + ggEZResponses.length + ' elements in responses');
+		console.log('i randomly picked the number ' + Math.random(ggEZResponses.length));
+		message.channel.send(ggEZResponses[Math.floor(Math.random() * ggEZResponses.length)]);
 	}
 }
 
