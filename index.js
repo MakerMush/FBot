2b2e1ff85d55596a832c7eaceab85bfe88f1643d
MakerMush <71267581+MakerMush@users.noreply.github.com>
2020-10-28 09:11:45 -0400

    added ggez

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const reactTo_F = ['😭', '😢', '🙁', '😞', '☹️', '😦', '):', ':(', ')=', '=(', 'd:', 'd='];
+const ggEZResponses = ['Wishing you all the best.', 'Well played, I salute you all.', 'For glory and honor! Huzzah comrades!', 'It was an honor to play with you all. Thank you.', 'Great game, everyone!', 'I feel very, very small... someone please hold me...', 'I\'m wrestling with some insecurity issues in my life but thank you all for playing with me.', 'Good game! Best of luck to you all!', 'C\'mon, Mom! One more game before you tuck me in. Oops mistell.', 'Gee whiz! That was fun. Good playing.', 'Ah shucks... you guys are the best!', 'I\'m trying to be a nicer person. It\'s hard, but I am trying, guys.', 'Mommy says people my age shouldn\'t suck their thumbs.', 'I could really use a hug right now.'];
 const lastFiveMessages = ['', '', '', '', ''];
 
 client.once('ready', () => {
@@ -19,6 +20,7 @@ client.on('message', message => {
 	checkForGameReactions(message);
 	checkForFReactions(message);
 	checkForServerDownMessages(message);
+	checkGGEZ(message);
 	checkForOtherReactions(message);
 
 });
@@ -287,6 +289,12 @@ function checkForFReactions(message) {
 
 		}
 
+	}
+}
+
+function checkGGEZ(message) {
+	if (message.content === 'ggez') {
+		message.channel.send(ggEZResponses[Math.random(ggEZResponses.length())]);
 	}
 }
 
